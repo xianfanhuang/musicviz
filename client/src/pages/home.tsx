3f2841cf8392b4c5bd5e0084a71dfafa4a2d12a7
xianfanhuang <18523689860@qq.com>
2025-09-14 15:48:04 +0800

    Delete client directory

--- client/src/pages/home.tsx
+++
@@ -1,9 +0,0 @@
-import MusicVisualizer from "@/components/music-visualizer";
-
-export default function Home() {
-  return (
-    <div className="min-h-screen overflow-hidden">
-      <MusicVisualizer />
-    </div>
-  );
-}
